826c5ba623fbe0f1649cdeb0568e264f6c9b2548
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-12 15:17:24 -0400

    SELECT names complete

--- .sql
+++ .sql
@@ -7,3 +7,37 @@ SELECT population FROM world
   
   SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000;
+  
+  -- SELECT names
+  SELECT name FROM world
+  WHERE name LIKE 'Y%';
+  
+  SELECT name FROM world
+  WHERE name LIKE '%y';
+  
+  SELECT name FROM world
+  WHERE name LIKE '%x%';
+  
+  SELECT name FROM world
+  WHERE name LIKE '%land';
+  
+  SELECT name FROM world
+  WHERE name LIKE 'C%ia';
+  
+  SELECT name FROM world
+  WHERE name LIKE '%oo%';
+  
+  SELECT name FROM world
+  WHERE name LIKE '%a%a%a%';
+  
+  SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name;
+
+SELECT name FROM world
+ WHERE name LIKE '%o__o%';
+ 
+ SELECT name FROM world
+ WHERE name LIKE '____';
+ 
+ 
