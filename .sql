cfaacf35b730a778be01371cfd4d9b90bf4d9a95
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-12 21:28:28 -0400

    SELECT world complete

--- .sql
+++ .sql
@@ -55,5 +55,42 @@ FROM world
 WHERE capital LIKE concat( '%', name, '%');
 
 
+
+-- SELECT from WORLD
+
+SELECT name, continent, population FROM world;
+
+SELECT name FROM world
+WHERE population>200000000;
+
+SELECT name, GDP/population FROM world
+WHERE population>200000000;
+
+SELECT name, population/1000000
+FROM world
+WHERE continent = 'South America';
+
+SELECT name, population FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+SELECT name country
+FROM world
+WHERE name LIKE '%United%';
+
+SELECT name, population, area FROM world
+WHERE population>250000000 OR area>3000000;
+
+SELECT name, population, area FROM world
+WHERE population>250000000 XOR area>3000000;
+
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2) FROM world
+WHERE continent = 'South America';
+
+SELECT name, ROUND(gdp/population, -3)
+FROM world
+WHERE gdp > 1000000000000;
+
+
+
  
  
